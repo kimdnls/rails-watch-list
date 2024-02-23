76b376d510bd491df533a5f23b9c34728e110a26
Kim Daniels <kimdaniels992@gmail.com>
2024-02-23 08:50:36 +0100

    watchlist

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-// app/assets/config/manifest.js
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link popper.js
 //= link bootstrap.min.js
